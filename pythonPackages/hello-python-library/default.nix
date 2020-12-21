f9f2e68276df3c80b3d43f8db3c99fbc3f94d8a0
Ubuntu <ubuntu@ip-172-31-17-154.eu-west-2.compute.internal>
2020-12-21 22:49:01 +0000

    added termcolorrr

--- pythonPackages/hello-python-library/default.nix
+++ pythonPackages/hello-python-library/default.nix
@@ -15,7 +15,7 @@ flox.pythonPackages.buildPythonPackage rec {
   # The propagated equivalent of buildInputs. Note that python modules
   # must always be listed in this section on account of the way that the
   # python module support works in Nix.
-  propagatedBuildInputs = [ ];
+  propagatedBuildInputs = [ pythonPackages.termcolor ];
 
   # See https://nixos.org/manual/nixpkgs/stable/#ssec-stdenv-dependencies for
   # more information on the various ways of specifying dependencies with Nix.
